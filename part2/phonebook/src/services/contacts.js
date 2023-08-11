cf05c687f3924fb290d7ab75a04eb523282d2021
Juan <juanfelipecardenas4@gmail.com>
2023-08-11 12:36:52 -0400

    Connected to API

--- part2/phonebook/src/services/contacts.js
+++ part2/phonebook/src/services/contacts.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3001/persons";
+const BASE_URL = "https://phonebook-api-5lr3.onrender.com/api/persons";
 
 const getAll = () => {
   return axios.get(BASE_URL);
 };
 
 const create = (newContact) => {
-    return axios.post(BASE_URL,newContact);
-}
+  return axios.post(BASE_URL, newContact);
+};
 
 const remove = (id) => {
-    return axios.delete(`${BASE_URL}/${id}`);
-}
+  return axios.delete(`${BASE_URL}/${id}`);
+};
 
 const update = (id, newContact) => {
-    return axios.put(`${BASE_URL}/${id}`,newContact);
-}
-export default { getAll, create, remove, update};
+  return axios.put(`${BASE_URL}/${id}`, newContact);
+};
+export default { getAll, create, remove, update };
